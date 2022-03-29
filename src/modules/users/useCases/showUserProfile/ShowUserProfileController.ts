5112f78fe3c5b86a6ec1cf0c1f8907dce218e805
Matheus Eduardo <matheus.eduardo.silva@ccc.ufcg.edu.br>
2022-03-29 17:25:19 -0300

    Resolvendo testes ShowUserProfile

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
-
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+
 
 class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { id } = request.body;
+    const user = this.showUserProfileUseCase.execute(id);
+    return response.send(user)
   }
 }
 
 export { ShowUserProfileController };
+
--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,8 +9,11 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if(!user) throw new Error("Usuário não encontrado.");
+    return user;
   }
 }
 
 export { ShowUserProfileUseCase };
+
